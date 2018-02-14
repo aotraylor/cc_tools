a0e7748ec7cbca2666e7ab7460b9c8b84c3ea28f
aotraylor <allisontraylor@Allisons-MacBook-Air.local>
2018-02-13 19:30:15 -0500

    part 2
    Added code to test_json_utils, convert, and made test_data.

--- convert.py
+++ convert.py
@@ -1,19 +1,59 @@
 import cc_dat_utils
+import example_data
+import test_json_utils
+import json
 
 #Part 1
-# Use cc_data_utils.make_cc_data_from_dat() to load pfgd_test.dat
+#Use cc_data_utils.make_cc_data_from_dat() to load pfgd_test.dat
+# cc_dat = cc_dat_utils.make_cc_data_from_dat("data/pfgd_test.dat")
+# print(cc_dat)
 #print the resulting data
-print(cc_dat_utils.make_cc_data_from_dat("data/pfgd_test.dat"))
+
+
+#Part 2 Example
+#Making the data structure manually
+# myFam = example_data.Family()
+# myFam.parent1 = "Dave"
+# myFam.parent2 = "Sabrina"
+#
+# myKid = example_data.Kid()
+# myKid.name = "Hazel"
+# myKid.age = 4
+# myFam.add_kid(myKid)
+# print("Family made in code:")
+# print(myFam)
+# print("")
+
+#Loading the data from a json file
+# example_json_file = "data/example_json.json"
+# with open(example_json_file, "r") as reader:
+#     family_data = json.load(reader)
+# print("JSON data:")
+# print(family_data)
+# print("")
+#
+# myFamFromJson = example_json_utils.make_family_from_json(family_data)
+# print("Family loaded from JSON:")
+# print(myFamFromJson)
+#End Part 2 Example
 
 
 #Part 2
 input_json_file = "data/test_data.json"
+with open(input_json_file, "r") as reader:
+    game_data = json.load(reader)
+
+print(game_data)
+testGameLibraryData = test_json_utils.make_game_library_from_json(game_data)
+
+print("Game list loaded from JSON:")
+print(testGameLibraryData)
 
 ### Begin Add Code Here ###
 #Open the file specified by input_json_file
 #Use the json module to load the data from the file
 #Use make_game_library_from_json(json_data) to convert the data to GameLibrary data
-#Print out the resulting GameLibrary data using print_game_library(game_library_data) in test_data.py
+#Print out the resulting GameLibrary data using print()
 ### End Add Code Here ###
 
 
--- test_json_utils.py
+++ test_json_utils.py
@@ -2,16 +2,37 @@ import test_data
 import json
 
 #Creates and returns a GameLibrary object(defined in test_data) from loaded json_data
-def make_game_library_from_json( json_data ):
+def make_game_library_from_json(json_data):
     #Initialize a new GameLibrary
     game_library = test_data.GameLibrary()
 
-    #Loop through the json_data
-        #Create a new Game object from the json_data by reading
+    # Loop through the json_data
+    for gameList_data in json_data:
+
+        #Create a new Game object
+        game = test_data.Game()
+        game.title = gameList_data["title"]
+        game.year = gameList_data["year"]
+
+        # making the platform object in game
+        game.platform = test_data.Platform()
+
+        #setting gamePlatform as an object to hold year and name
+        gamePlatform = gameList_data["platform"];
+
+        #attributes of the object platform
+        game.platform.launch_year = gamePlatform["launch year"]
+        game.platform.name = gamePlatform["name"]
+
+        #Add that Game object to the game_library
+        game_library.add_game(game)
+
+
+
         #  title
         #  year
         #  platform (which requires reading name and launch_year)
-        #Add that Game object to the game_library
+
     #Return the completed game_library
 
     return game_library
